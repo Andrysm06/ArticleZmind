d4c0728efd371cb06da975b669622e22ea3c7bb0
Andrysm06 <andrysetiawanm2206@gmail.com>
2024-11-06 20:16:56 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Zmind from "../../public/image/ZmindLogo.png";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons"; // Import Font Awesome bars icon
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to toggle the menu visibility
   const navigate = useNavigate();
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <nav className="bg-white bg-opacity-60 backdrop-blur-md text-black p-4 shadow-md fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
@@ -24,13 +16,7 @@ const Navbar = () => {
           />
         </div>
 
-        <div className="md:hidden">
-          <button onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} className="text-black text-2xl" />
-          </button>
-        </div>
-
-        <div className="hidden md:flex space-x-6">
+        <div className="flex space-x-6">
           <a
             href="/"
             className="text-black hover:text-gray-600 transition duration-300"
@@ -44,39 +30,13 @@ const Navbar = () => {
             Kontak Kami
           </a>
           <a
-            href="/Zmind"
+            href="/About"
             className="text-black hover:text-gray-600 transition duration-300"
           >
             Tentang Kami
           </a>
         </div>
       </div>
-
-      {isMenuOpen && (
-        <div className="md:hidden flex flex-col space-y-4 mt-4 px-4 py-2 bg-white shadow-md rounded-lg">
-          <a
-            href="/"
-            className="text-black hover:text-gray-600 transition duration-300"
-            onClick={() => setIsMenuOpen(false)} // Close menu when an item is clicked
-          >
-            Artikel
-          </a>
-          <a
-            href="#projects"
-            className="text-black hover:text-gray-600 transition duration-300"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Kontak Kami
-          </a>
-          <a
-            href="/Zmind"
-            className="text-black hover:text-gray-600 transition duration-300"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Tentang Kami
-          </a>
-        </div>
-      )}
     </nav>
   );
 };
